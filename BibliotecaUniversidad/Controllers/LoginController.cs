fa8e7bcfc55c1eb4017cccea09ad0499e45bbe8f
SmerlinFernandez <smerlinfernandez1@gmail.com>
2023-10-31 20:16:55 -0400

    Agregar archivos de proyecto.

---
+++ BibliotecaUniversidad/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BibliotecaUniversidad.Data;
+
+namespace BibliotecaUniversidad.Controllers
+{
+    public class LoginController : Controller
+    {
+        BibliotecaEntities db = new BibliotecaEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult Index(Usuario user)
+        {
+            var myuser = db.Usuario.FirstOrDefault(u => u.username == user.username
+                    && u.pass == user.pass);
+
+            if (myuser != null)    
+            {
+                return View(user);
+            }
+            else    
+            {
+                return View();
+            }
+            
+        }
+    }
+}
